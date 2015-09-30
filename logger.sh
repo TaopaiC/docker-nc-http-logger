9982aae416de369a39df0759d5d9b932979129c8
TaopaiC <pctao.tw@gmail.com>
2015-10-01 02:25:08 +0800

    use corrected logger.sh

--- logger.sh
+++ logger.sh
@@ -1,4 +1,2 @@
 #!/bin/env sh
-#while true ; do nc -l -p 80  < index.html; test $? -gt 128 && break; done
-# while true ; do nc -l -p 80  < index.html; echo $?; done
-nc -l -p 80 < index.html
+while true ; do nc -l -p 80  < index.html; echo $?; done
